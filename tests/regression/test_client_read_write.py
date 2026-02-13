d31beb3064c288dd162fe5cf438c0408439f3bed
Ryan Soley <ryan.e.soley@gmail.com>
2026-02-13 13:59:57 -0500

    test: add client regression tests

---
+++ tests/regression/test_client_read_write.py
@@ -0,0 +1,88 @@
+import contextlib
+import os
+import tempfile
+import time
+
+import pytest
+
+from rubicon_ml import Rubicon
+
+
+TAGS_TO_ADD = ["added_tag_a", "added_tag_b"]
+TAGS_TO_REMOVE = ["added_tag_a"]
+COMMENTS_TO_ADD = ["added_comment_a", "added_comment_b"]
+COMMENTS_TO_REMOVE = ["added_comment_a"]
+
+CLIENTS_TO_TEST = [
+    pytest.param("filesystem"),
+    pytest.param("memory"),
+    pytest.param("wandb", marks=pytest.mark.wandb),
+]
+
+
+def _test_read_write_additional_tags_and_comments(entity):
+    """Test adding and removing tags and comments on an entity via client."""
+    is_passing = True
+
+    original_tags = list(entity.tags)
+    entity.add_tags(TAGS_TO_ADD)
+    entity.remove_tags(TAGS_TO_REMOVE)
+
+    current_tags = list(entity.tags)
+    expected_tags = original_tags + [t for t in TAGS_TO_ADD if t not in TAGS_TO_REMOVE]
+    is_passing &= set(current_tags) == set(expected_tags)
+
+    original_comments = list(entity.comments)
+    entity.add_comments(COMMENTS_TO_ADD)
+    entity.remove_comments(COMMENTS_TO_REMOVE)
+
+    current_comments = list(entity.comments)
+    expected_comments = original_comments + [c for c in COMMENTS_TO_ADD if c not in COMMENTS_TO_REMOVE]
+    is_passing &= set(current_comments) == set(expected_comments)
+
+    return is_passing
+
+
+@pytest.mark.parametrize("persistence", CLIENTS_TO_TEST)
+def test_read_write_project_client_regression(project_parameters, persistence):
+    """Tests that `rubicon_ml` client can read the project entity that it wrote."""
+    if persistence == "filesystem":
+        temp_dir_context = tempfile.TemporaryDirectory()
+    else:
+        temp_dir_context = contextlib.nullcontext(
+            enter_result="./test_read_write_project_client_regression/"
+        )
+
+    with temp_dir_context as temp_dir_name:
+        root_dir = os.path.join(temp_dir_name, "test-rubicon-ml")
+        rubicon = Rubicon(persistence=persistence, root_dir=root_dir)
+        project = rubicon.create_project(**project_parameters)
+
+        retrieved_project = rubicon.get_project(project.name)
+
+        assert retrieved_project._domain == project._domain
+
+
+@pytest.mark.parametrize("persistence", CLIENTS_TO_TEST)
+def test_read_write_experiment_client_regression(experiment_parameters, project_parameters, persistence):
+    """Tests that `rubicon_ml` client can read the experiment entity that it wrote."""
+    if persistence == "filesystem":
+        temp_dir_context = tempfile.TemporaryDirectory()
+    else:
+        temp_dir_context = contextlib.nullcontext(
+            enter_result="./test_read_write_experiment_client_regression/"
+        )
+
+    with temp_dir_context as temp_dir_name:
+        root_dir = os.path.join(temp_dir_name, "test-rubicon-ml")
+        rubicon = Rubicon(persistence=persistence, root_dir=root_dir)
+        project = rubicon.create_project(**project_parameters)
+        experiment = project.log_experiment(**experiment_parameters)
+
+        if persistence == "wandb":
+            time.sleep(2)  # allow `wandb` time to complete sync
+
+        retrieved_experiment = project.experiment(id=experiment.id)
+
+        assert retrieved_experiment._domain == experiment._domain
+        assert _test_read_write_additional_tags_and_comments(retrieved_experiment)
